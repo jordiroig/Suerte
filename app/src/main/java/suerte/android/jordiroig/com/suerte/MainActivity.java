60dae307b860e1c4c4bdaec30e87565cbdf3a752
Jordi <jordi@jordiroig.com>
2015-03-25 14:12:40 +0100

    first activity

--- app/src/main/java/suerte/android/jordiroig/com/suerte/MainActivity.java
+++ app/src/main/java/suerte/android/jordiroig/com/suerte/MainActivity.java
@@ -4,16 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button main_button;
+    EditText main_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_button = (Button)findViewById(R.id.main_button);
+        main_input = (EditText)findViewById(R.id.main_input);
+        main_button.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        int targetId = v.getId();
+        if(targetId == R.id.main_button) //No haria falta, ya que solo hemos creado un listener
+        {
+            if (main_input.getText().length() > 0) { //Comprobamos que se ha introducido algún valor
+                //Vamos al siguiente activity
+            } else
+            {
+                Toast.makeText(getApplicationContext(), "Debe introducir un valor", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +60,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
