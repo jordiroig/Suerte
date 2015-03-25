a1488fb1e56e43f19f84b03b6b51951ca509e35e
Jordi <jordi@jordiroig.com>
2015-03-25 13:18:47 +0100

    Initial commit

---
+++ app/src/androidTest/java/suerte/android/jordiroig/com/suerte/ApplicationTest.java
@@ -0,0 +1,13 @@
+package suerte.android.jordiroig.com.suerte;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
